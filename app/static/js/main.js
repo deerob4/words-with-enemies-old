5b7ae1b6f3a9205a619c8090d4ce6bb423b722f7
Dee Roberts <deerob4@gmail.com>
2015-02-15 23:33:42 +0000

    Start work on setup page - music and rotate animation.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,19 @@
-/**
- * Created by dee on 15/02/15.
- */
+$(document).ready(function() {
+    $('h1').click(function() {
+        $.ajax({
+            url: '_ajax/get_letters',
+            type: 'POST',
+            success: function(data) {
+                alert(data['letters'])
+            }
+        })
+    });
+
+    setTimeout(function() {
+        $('.setup-text').addClass('rotate');
+    }, 1000);
+
+    var bgMusic = new buzz.sound('../static/sounds/space_adventure.mp3');
+    bgMusic.play().loop().fadeIn();
+
+});
--- app/words_with_enemies.py
+++ app/words_with_enemies.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from app.generate import random_letters
 
 app = Flask(__name__)
 
@@ -8,6 +9,12 @@ app.config['SECRET_KEY'] = "Q?FVpu)adW>P&4N&E2+ou9>'c"
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/_ajax/get_letters', methods=['POST'])
+def get_letters():
+    print(random_letters(10))
+    return jsonify(letters=random_letters(10))
 
 
 if __name__ == '__main__':
