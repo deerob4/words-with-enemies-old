cfb75584ea24389549ed1a6a7230a3fa0ce49a92
Dee Roberts <deerob4@gmail.com>
2015-02-17 20:54:37 +0000

    Add hint messages. Add ability to change colour theme - woo! Add the most gorgeous buttons ever seen. Add ability to check word is real. More animations. Added ability to add two additional letters per turn.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,15 +3,11 @@ $(document).ready(function () {
     var bgMusic = new buzz.sound('../static/sounds/sand_castles.mp3');
     bgMusic.play().loop().fadeIn();
 
-    var colourChoices = ['blue', 'red', 'orange', 'purple'];
-    var mainColour = randomColor({
-        luminosity: 'light',
-        hue: colourChoices[Math.floor(Math.random() * colourChoices.length)]
-    });
-
     var currentWord = [];
+    var wordMessage = true;
+    var lettersAdded = 0;
 
-    var letters = document.getElementById('letters');
+    var letters = document.getElementById('formed-word');
     var sortable = new Sortable(letters, {
         sort: true,
         draggable: '.letter',
@@ -19,10 +15,16 @@ $(document).ready(function () {
         ghostClass: 'letter-ghost'
     });
 
-
     function calculateColours() {
+        var colourChoices = ['blue', 'red', 'orange', 'purple'];
+        var mainColour = randomColor({
+            luminosity: 'light',
+            hue: colourChoices[Math.floor(Math.random() * colourChoices.length)]
+        });
+        console.log('Main colour chosen.');
         var gameColour = colorLuminance(mainColour, -0.2);
-        //Main menu colours
+        console.log('Game colour chosen.');
+
         $('.setup h1').css('color', gameColour);
         $('.setup p').css('color', mainColour);
         var buttonColour = colorLuminance(mainColour, -0.3);
@@ -35,14 +37,19 @@ $(document).ready(function () {
 
         $('.line').css('background-color', mainColour);
         $('.username').css('color', userColour);
-        $('.round-number').css('color', randomColor({luminosity: 'light'}));
+        $('.round-number').css('color', mainColour);
         $('.computer').css('color', computerColour);
+        $('.game-buttons div').css('background-color', mainColour).css('border-color', gameColour).css('color', gameColour);
 
         var userWordBackground = colorLuminance(userColour, 0.1);
-        $('.formed-word').css('background-color', userWordBackground).css('border-color', colorLuminance(userWordBackground, -0.2));
+        var userWordBorder = colorLuminance(userWordBackground, -0.2);
+        $('.formed-word').css('background-color', userWordBackground).css('border-color', userWordBorder);
+        $('.word-message').css('color', userWordBorder);
 
         var computerWordBackground = colorLuminance(computerColour, 0.1);
-        $('.computer-word').css('background-color', computerWordBackground).css('border-color', colorLuminance(computerWordBackground, -0.2));
+        var computerWordBorder = colorLuminance(computerWordBackground, -0.2);
+        $('.computer-word').css('background-color', computerWordBackground).css('border-color', computerWordBorder);
+        $('.word-message-computer').css('color', computerWordBorder);
     }
 
     function mainMenu() {
@@ -90,7 +97,7 @@ $(document).ready(function () {
         for (var i = 0; i <= letterset.length - 1; i++) {
             var mainColour = randomColor({luminosity: 'light'});
             var borderColour = colorLuminance(mainColour, -0.2);
-            $('.letters').append('<li class="letter" id=' + i + '" style="background-color: ' + mainColour + '; border: 3px solid ' + borderColour + ';">' + letterset[i] + '</li>');
+            $('.letters').append('<li class="letter" id=' + i + '" style="background-color: ' + mainColour + '; border: 3px solid ' + borderColour + '; color: ' + colorLuminance(borderColour, -0.2) + '">' + letterset[i] + '</li>');
         }
     }
 
@@ -107,12 +114,32 @@ $(document).ready(function () {
         })
     }
 
+    function checkDictionary(word) {
+        $.ajax({
+            url: '/_ajax/check_dictionary',
+            type: 'POST',
+            data: word,
+            success: function (data) {
+                if (data == 'valid') {
+                    genericAnimation($('.formed-word'), 'rubberBand', true);
+                } else {
+                    genericAnimation($('.formed-word'), 'wobble', true);
+                    changeComputerText('Sorry, that isn\'t a real word!', 2000);
+                }
+            }
+        });
+    }
+
     function addToWord($letter) {
         var letter = $letter.text();
         if (!$letter.hasClass('added')) {
+            if (wordMessage == true) {
+                $('.word-message').remove();
+                wordMessage = false;
+            }
             currentWord.push(letter);
             $letter.addClass('added');
-            $('.formed-word').append('<li class="letter" id=' + letter + '" style="background-color: ' + $letter.css('background-color') + '; border: 3px solid ' + $letter.css('border-color') + ';">' + letter + '</li>');
+            $('.formed-word').append('<li class="letter" id="' + letter + '" style="background-color: ' + $letter.css('background-color') + '; border: 3px solid ' + $letter.css('border-color') + '; color: ' + $letter.css('border-color') + '">' + letter + '</li>');
             $letter.remove();
         } else {
             remove(currentWord, letter);
@@ -123,7 +150,7 @@ $(document).ready(function () {
 
     function addToTray($letter) {
         var letter = $letter.text();
-        $('.letters').append('<li class="letter" id=' + letter + '" style="background-color: ' + $letter.css('background-color') + '; border: 3px solid ' + $letter.css('border-color') + ';">' + letter + '</li>');
+        $('.letters').append('<li class="letter" id=' + letter + '" style="background-color: ' + $letter.css('background-color') + '; border: 3px solid ' + $letter.css('border-color') + '; color: ' + $letter.css('border-color') + '">' + letter + '</li>');
         $letter.remove();
     }
 
@@ -155,12 +182,79 @@ $(document).ready(function () {
         }
     }
 
+    function genericAnimation($element, animation, timeout) {
+        $element.addClass('animated ' + animation);
+        if (timeout == true) {
+            setTimeout(function () {
+                $element.removeClass('animated ' + animation);
+            }, 1400);
+        }
+    }
+
+    function changeComputerText(message, timeout) {
+        $('.word-message-computer').text(message);
+        setTimeout(function () {
+            $('.word-message-computer').text('Computer\'s word goes here!');
+        }, timeout)
+    }
+
     $('.formed-word').on('click', '.letter', function () {
         addToTray($(this));
     });
 
     $('.letters').on('click', '.letter', function () {
         addToWord($(this));
+    });
+
+    $('.finished').click(function () {
+        currentWord.length = 0;
+        $('.formed-word .letter').each(function () {
+            currentWord.push($(this).text())
+        });
+        if (currentWord.length > 0) {
+            checkDictionary(currentWord.toString());
+        } else {
+            genericAnimation($('.formed-word'), 'wobble', true);
+        }
+
+    });
+
+    $('.change-colours').click(function () {
+        calculateColours();
+        $('.letters .letter').each(function () {
+            var background = randomColor({luminosity: 'light'});
+            var border = colorLuminance(background, -0.2);
+            $(this).css('background-color', background).css('border-color', border).css('color', colorLuminance(border, -0.2));
+        });
+        $('.formed-word .letter').each(function () {
+            var background = randomColor({luminosity: 'light'});
+            var border = colorLuminance(background, -0.2);
+            $(this).css('background-color', background).css('border-color', border).css('color', colorLuminance(border, -0.2));
+        });
+    });
+
+    $('.vowel').click(function () {
+        if (lettersAdded < 2) {
+            var vowels = ['a', 'e', 'i', 'o', 'u'];
+            var letterset = [vowels[Math.floor(Math.random() * vowels.length)]];
+            displayLetters(letterset);
+            lettersAdded++;
+        } else {
+            genericAnimation($(this), 'shake', true);
+            changeComputerText('You can only have two extra letters per round!', 2000);
+        }
+    });
+
+    $('.consonant').click(function () {
+        if (lettersAdded < 2) {
+            var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y'];
+            var letterset = [consonants[Math.floor(Math.random() * consonants.length)]];
+            displayLetters(letterset);
+            lettersAdded++;
+        } else {
+            genericAnimation($(this), 'shake', true);
+            changeComputerText('You can only have two extra letters per round!', 2000);
+        }
     });
 
     calculateColours();
--- app/words_with_enemies.py
+++ app/words_with_enemies.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, jsonify, request
-from app.generate import random_letters
 from getpass import getuser
+from random import choice
 
 app = Flask(__name__)
+
+with open('../sowpods.txt', 'r') as f:
+    word_list = [line.lower().replace('\n', '') for line in f]
+
+
+def check_dictionary(user_word):
+    for word in word_list:
+        if word == user_word:
+            return True
+    else:
+        return False
+
+
+def random_letters(n):
+    vowels = 'aeiou'
+    consonants = 'bcdfghjklmnpqrstvwxyz'
+    num_vowels = n // 3
+    lettersets = [vowels] * num_vowels + [consonants] * (n - num_vowels)
+    return [letter for letterset in lettersets for letter in choice(letterset)]
 
 
 @app.route('/')
@@ -15,6 +34,17 @@ def get_letters():
     difficulty_map = {'easy': 15, 'medium': 10, 'hard': 6}
     difficulty = request.get_data().decode("utf-8")
     return jsonify(letterset=random_letters(difficulty_map[difficulty]))
+
+
+@app.route('/_ajax/check_dictionary', methods=['POST'])
+def check_dictionary():
+    user_word = request.get_data().decode("utf-8").replace(',', '')
+    print(user_word)
+    for word in word_list:
+        if word == user_word:
+            return 'valid'
+    else:
+        return 'invalid'
 
 
 if __name__ == '__main__':
